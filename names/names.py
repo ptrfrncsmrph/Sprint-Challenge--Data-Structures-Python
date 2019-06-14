0677e26e2fe6445725f4dd93ed52a1b62dbffcbd
Peter Murphy <peter.francis.murphy@gmail.com>
2019-06-14 11:55:29 -0400

    Optimize names.py in O(n) time

--- names/names.py
+++ names/names.py
@@ -2,21 +2,15 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+with open('names_1.txt', 'r') as f1, open('names_2.txt', 'r') as f2:
+    duplicates = []
+    hash = {}
+    for line in f1:
+        hash[line] = True
+    for line in f2:
+        if line in hash:
+            duplicates.append(line.strip())
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
